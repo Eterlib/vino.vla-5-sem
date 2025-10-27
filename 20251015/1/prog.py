3084d6612f253880662025518d3fef62dd2bf323
Vladimir Vinogradov <fes1z1515@gmail.com>
2025-10-27 14:11:18 +0300

    Izmeneno check

--- 20251015/1/prog.py
+++ 20251015/1/prog.py
@@ -1,29 +1,38 @@
 from fractions import Fraction
 
-def parse_num(s):
-    if '/' in s:
-        return Fraction(s)
-    else:
-        return Fraction(int(s),1)
+def to_frac(v):
+    if isinstance(v, Fraction): return v
+    if isinstance(v, (int, float)): return Fraction(v).limit_denominator()
+    v = v.strip()
+    return Fraction(v)
 
 def poly_val(coeffs, x):
-    res = Fraction(0,1)
-    for c in reversed(coeffs):
+    res = Fraction(0, 1)
+    for c in coeffs:
         res = res * x + c
     return res
 
-def check(s,w,degA,coeffsA,degB,coeffsB):
-    x = parse_num(s)
-    rhs = parse_num(w)
+def check(s, w, degA, coeffsA, degB, coeffsB):
+    x = to_frac(s)
+    rhs = to_frac(w)
     A = poly_val(coeffsA, x)
     B = poly_val(coeffsB, x)
-    if B==0: return False
-    return A/B == rhs
+    if B == 0:
+        return False
+    return A / B == rhs
 
-data = input().replace(',',' ').split()
-s = data[0]; w = data[1]
-degA = int(data[2]); coeffsA = [parse_num(x) for x in data[3:3+degA+1]]
-pos = 3+degA+1
-degB = int(data[pos]); coeffsB = [parse_num(x) for x in data[pos+1:pos+1+degB+1]]
+tokens = input().replace(',', ' ').split()
 
-print(check(s,w,degA,coeffsA,degB,coeffsB))
+s, w = tokens[0], tokens[1]
+
+degA = int(tokens[2])
+a_start = 3
+a_end = a_start + degA + 1
+coeffsA = [to_frac(t) for t in tokens[a_start:a_end]]
+
+degB = int(tokens[a_end])
+b_start = a_end + 1
+b_end = b_start + degB + 1
+coeffsB = [to_frac(t) for t in tokens[b_start:b_end]]
+
+print(check(s, w, degA, coeffsA, degB, coeffsB))
