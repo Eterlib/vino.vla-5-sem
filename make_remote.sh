25e4f1c37b8b89f06c6314019cfac429125fbeab
Vladimir Vinogradov <fes1z1515@gmail.com>
2025-12-23 00:07:40 +0300

    remote

---
+++ make_remote.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# запускать из корневой директории вашего гит-репозитория
+
+HIM="Авдонин Алексей"
+BASE_DIR=$(pwd)
+
+find "$BASE_DIR" -type d -name "check" | grep "2025" | while read -r CHECK_DIR; do
+    FILE_PATH="$CHECK_DIR/remote"
+
+    # Получаем относительный путь без GNU realpath
+    RELATIVE_PATH="${CHECK_DIR#$BASE_DIR/}"
+    RELATIVE_PATH="${RELATIVE_PATH%/check}"
+
+    cat << EOF > "$FILE_PATH"
+[remote]
+"$HIM:$RELATIVE_PATH/1" = []
+"$HIM:$RELATIVE_PATH/2" = []
+"$HIM:$RELATIVE_PATH/3" = []
+EOF
+
+    echo "Файл создан: $FILE_PATH"
+done
+
